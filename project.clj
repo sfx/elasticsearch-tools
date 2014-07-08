6949225eba1b7d1ff22d2c04784060f08ca05b4f
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2014-07-08 13:36:47 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject elasticsearch-tools "0.1.0"
   :description "Tools for Dealing with ElasticSearch."
-  :url ""
+  :url "https://github.com/sfx/elasticsearch-tools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
